d764874a6bb99ac5a36b7208ab8f4a7336c61244
thamshid <thamshidcc@gmail.com>
2016-08-29 15:22:18 +0530

    group level added in fixture

--- QBid_server/generate_fixture.py
+++ QBid_server/generate_fixture.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
         matches = list(itertools.permutations(teams, 2))
         for match in matches:
             match_obj = Match()
+            match_obj.name = 'Group level'
             match_obj.team1 = match[0]
             match_obj.team2 = match[1]
             match_obj.save()
